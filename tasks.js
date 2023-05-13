8b4ecb8d399f5f8b5d0df20cd2da70faa1d2c7bc
Mark-develop <markleventsow@gmail.com>
2023-05-13 19:14:19 +0200

    View modified
    Formalization of code and output to the console

--- tasks.js
+++ tasks.js
@@ -21,18 +21,23 @@ console.log('');
 console.log('FIND VOWELS 1');
 console.log('This is first way to find volwes in string');
 //
-console.log(FindVowels('Earials'));  
+
+console.log('Earials');
+console.log(FindVowels('Earials'));
+console.log('Why');
 console.log(FindVowels('Why'));  
 
 function FindVowels(str){
    let count = 0;
    const vowels = ['a','e','i','o','u'];
+   console.log(str);
 
    for (let char of str) {
        if (vowels.includes(char)){
            count += 1;
        }
    };
+   
    return  count
 }
 
@@ -41,7 +46,9 @@ console.log('');
 console.log('FIND VOWELS 2');
 console.log('This is second way to find volwes in string');
 //
+console.log('Earials');
 console.log(FindVowels('Earials'));  
+console.log('Why');
 console.log(FindVowels('Why'));  
 
 function FindVowels(str){
@@ -50,7 +57,10 @@ function FindVowels(str){
 }
 
 //    FIZZBUZZ 1 *********
-FizzBuzz(22);
+console.log('');
+console.log('This method print numbers from 1 to 30. Number multiple of 3 print "fizz", and print "Buzz" for multiples of 5, and lastly print "FizzBuzz" for multiples of 3 and 5 ');
+
+FizzBuzz(30);
 
 function FizzBuzz(num){
    for (let i = 1; i <= num; i++) {
@@ -68,16 +78,18 @@ function FizzBuzz(num){
    }
 }
 
- // FIZZBUZZ 2 *********
-FizzBuzz(22);
+ // this is just another way to write logic "if else" for a previous method. It takes up less space but is less readable
+ console.log('')
+ console.log('this is just another way to write logic "if else" for a previous method. It takes up less space but is less readable:');
+ console.log('function FizzBuzzz(num){\nfor (let i = 1; i <= num; i++) {\ni % 3 === 0 && i % 5  === 0 ? console.log("FizzBuzz") :\ni % 3 === 0 ? "Fizz" : \ni % 5 === 0 ? "Buzz" : "i"; \n}}')
 
-function FizzBuzz(num){
-  for (let i = 1; i <= num; i++) {
-       i % 3 === 0 && i % 5  === 0 ? console.log('FizzBuzz') :
-       i % 3 === 0 ? console.log('Fizz') :
-       i % 5 === 0 ? console.log('Buzz') : console.log(i);
-  }
-}   
+// function FizzBuzzz(num){
+//   for (let i = 1; i <= num; i++) {
+//        i % 3 === 0 && i % 5  === 0 ? console.log('FizzBuzz') :
+//        i % 3 === 0 ? "Fizz" :
+//        i % 5 === 0 ? "Buzz" : "i";
+//   }
+// }
 
 // //ANAGRAMM ******
 
